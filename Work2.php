ea6fd8f29f4d27da03876affb071fccd7302e7b2
Абиняков <danabinar@gamil.com>
2025-02-16 16:49:43 +0300

    Три файла

---
+++ Work2.php
@@ -0,0 +1,16 @@
+<?php
+function getTriangleType($a, $b, $c) {
+    if ($a + $b <= $c || $a + $c <= $b || $b + $c <= $a) {
+        return "Не треугольник";
+    }
+    
+    $type = ($a == $b && $b == $c) ? "Равносторонний" : (($a == $b || $b == $c || $a == $c) ? "Равнобедренный" : "Разносторонний");
+    
+    return $type;
+}
+
+$a = (int) readline("Введите сторону A: ");
+$b = (int) readline("Введите сторону B: ");
+$c = (int) readline("Введите сторону C: ");
+
+echo "Тип треугольника: " . getTriangleType($a, $b, $c) . "\n";
